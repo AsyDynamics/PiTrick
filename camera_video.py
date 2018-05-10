1bd31ebcbb12b460f9ce10c4fff2988fed91c64d
AsyDynamics <shuyangan.seu@gmail.com>
2018-05-10 10:22:42 +0200

    Modify the RPI temp script, make it executable

---
+++ camera_video.py
@@ -0,0 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+camera=PiCamera()
+camera.rotation=180
+camera.resolution=(1920,1080)
+camera.framerate=15
+camera.start_preview()
+camera.start_recording('/home/pi/Videos/video.h264')
+sleep(8)
+camera.stop_recording()
+camera.stop_preview()
